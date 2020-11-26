0beffd0f1b575599a544c7767a9985d318817d21
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-11-26 18:04:00 +0800

    commit 3

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
@@ -8,6 +8,10 @@ export default function App() {
       <Text>Op GG</Text>
       <Text>Whooper</Text>
       <Text>Sick lad</Text>
+      <Button title="Get WHOOPED" />
+      <TouchableOpacity style={styles.button}>
+        <Text>Get Whooped 2</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,8 +20,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "coral",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "space-around",
+  },
+  button: {
+    backgroundColor: "red",
+    padding: 20,
   },
 });
