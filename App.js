d341d7c0e9a66c7f32ae8dbc9fa90a2922e91143
limZyuan <58379837+limZyuan@users.noreply.github.com>
2020-11-27 12:26:53 +0800

    update

--- App.js
+++ App.js
@@ -1,29 +1,79 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  Alert,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import Header from "./components/header";
+import TodoItem from "./components/todoItem";
+import AddTodo from "./components/addTodo";
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "buy coffee",
+      key: "1",
+    },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
+  ]);
+
+  const pressHandler = (key) => {
+    setTodos((prevState) => {
+      return prevState.filter((item) => item.key !== key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevState) => {
+        return [{ text, key: Math.random().toString() }, ...prevState];
+      });
+    } else {
+      Alert.alert("Oops!", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ]);
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Press to get a whooping</Text>
-      <Button title="Get WHOOPED" />
-      <TouchableOpacity style={styles.button}>
-        <Text>Get Whooped 2</Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+        console.log("dismissed keyboard");
+      }}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler} />
+          <View style={styles.list}>
+            <FlatList
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "space-around",
+    backgroundColor: "#fff",
   },
-  button: {
-    backgroundColor: "coral",
-    padding: 20,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
---
+++ components/addTodo.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, Button, TextInput, View } from "react-native";
+
+const AddToDo = ({ submitHandler }) => {
+  const [text, setText] = useState("");
+
+  const changeHandler = (val) => {
+    setText(val);
+  };
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        placeholder="new todo..."
+        onChangeText={changeHandler}
+      />
+      <Button
+        onPress={() => submitHandler(text)}
+        title="add todo"
+        color="coral"
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    margin: 10,
+    paddingHorizontal: 8,
+    paddingVertical: 6,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+});
+
+export default AddToDo;
